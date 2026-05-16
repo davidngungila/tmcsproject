9b04b560ff86614585c2af2ef687f8e701821acd
David Ngungila <davidngungila@gmail.com>
2026-05-16 08:18:41 +0300

    fix: resolve ID card PDF page overflow and ensure strict 2-page output

--- resources/views/members/id_card_pdf.blade.php
+++ resources/views/members/id_card_pdf.blade.php
@@ -7,6 +7,10 @@
             size: 85.6mm 53.98mm;
             margin: 0; 
         }
+        * {
+            box-sizing: border-box;
+            -webkit-print-color-adjust: exact;
+        }
         body {
             font-family: 'Helvetica', sans-serif;
             margin: 0;
@@ -14,48 +18,47 @@
             background: #fff;
             width: 85.6mm;
             height: 53.98mm;
-        }
-        .page-break {
-            page-break-after: always;
+            line-height: 1;
         }
         .id-card {
             width: 85.6mm;
             height: 53.98mm;
             position: relative;
             overflow: hidden;
-            box-sizing: border-box;
+            display: block;
+            page-break-after: always;
+        }
+        .id-card:last-child {
+            page-break-after: avoid;
         }
         .header-section {
             height: 14mm;
             background: #064e3b;
             color: white;
             padding: 0 4mm;
-            display: table;
             width: 100%;
-            box-sizing: border-box;
+            overflow: hidden;
         }
         .church-info {
-            display: table-cell;
-            vertical-align: middle;
-            padding-left: 2mm;
+            padding-top: 2.5mm;
         }
         .church-info h1 {
             margin: 0;
-            font-size: 2.6mm;
+            font-size: 2.5mm;
             font-weight: bold;
             text-transform: uppercase;
-            line-height: 1.2;
+            color: #ffffff;
         }
         .church-info p {
             margin: 0;
             font-size: 1.4mm;
             opacity: 0.9;
-            line-height: 1.2;
+            color: #ffffff;
         }
         .card-main {
             padding: 3mm 4mm;
             width: 100%;
-            box-sizing: border-box;
+            height: 39.98mm;
         }
         .photo-container {
             width: 22mm;
@@ -63,22 +66,22 @@
             float: left;
             margin-right: 3mm;
             border: 0.4mm solid #059669;
-            box-sizing: border-box;
+            background: #f3f4f6;
         }
         .photo-container img {
             width: 100%;
             height: 100%;
-            object-fit: cover;
+            display: block;
         }
         .member-details {
             float: left;
-            width: 50mm;
+            width: 52mm;
         }
         .name-tag h2 {
             margin: 0;
             font-size: 3.2mm;
             color: #111827;
-            line-height: 1.1;
+            font-weight: bold;
         }
         .name-tag span {
             font-size: 1.8mm;
@@ -92,7 +95,7 @@
             margin-top: 1.5mm;
         }
         .info-item {
-            margin-bottom: 0.8mm;
+            margin-bottom: 1mm;
         }
         .info-item label {
             display: block;
@@ -103,16 +106,18 @@
         }
         .info-item p {
             margin: 0;
-            font-size: 1.8mm;
+            font-size: 1.9mm;
             font-weight: bold;
             color: #111827;
-            line-height: 1;
         }
         .valid-thru {
-            margin-top: 1.5mm;
+            margin-top: 2mm;
             font-size: 1.6mm;
             font-weight: bold;
             color: #ef4444;
+            background: #fee2e2;
+            padding: 0.4mm 1mm;
+            display: inline-block;
         }
 
         /* BACK SIDE */
@@ -120,7 +125,6 @@
             background: #111827;
             color: white;
             padding: 4mm 4mm;
-            box-sizing: border-box;
         }
         .back-title h3 {
             margin: 0;
@@ -129,30 +133,35 @@
             text-transform: uppercase;
         }
         .qr-section {
-            margin-top: 2.5mm;
+            margin-top: 3mm;
             width: 100%;
-            display: table;
+            height: 22mm;
         }
         .qr-box {
             width: 20mm;
             height: 20mm;
             background: white;
             padding: 1mm;
-            display: table-cell;
-            vertical-align: top;
+            float: left;
+        }
+        .qr-box img {
+            width: 18mm;
+            height: 18mm;
+            display: block;
         }
         .terms {
-            display: table-cell;
-            vertical-align: top;
+            float: left;
+            width: 53mm;
             padding-left: 3mm;
             font-size: 1.4mm;
             color: #9ca3af;
-            line-height: 1.4;
+            line-height: 1.3;
         }
         .terms strong { color: white; display: block; margin-bottom: 0.5mm; font-size: 1.6mm; }
         .signature-section {
             margin-top: 3mm;
             width: 100%;
+            clear: both;
         }
         .sig-box {
             width: 35mm;
@@ -163,7 +172,7 @@
             border-top: 0.1mm solid #fff;
             margin-bottom: 0.5mm;
         }
-        .sig-label { font-size: 1.4mm; opacity: 0.7; }
+        .sig-label { font-size: 1.4mm; opacity: 0.7; color: #ffffff; }
         .footer-text {
             position: absolute;
             bottom: 2mm;
@@ -217,8 +226,6 @@
         </div>
     </div>
 
-    <div class="page-break"></div>
-
     <!-- BACK -->
     <div class="id-card card-back">
         <div class="back-title">
@@ -233,7 +240,7 @@
                     $qrContent .= "NAME: " . $member->full_name . "\n";
                     $qrContent .= "REG NO: " . $member->registration_number;
                 @endphp
-                <img src="data:image/png;base64, {!! base64_encode(\SimpleSoftwareIO\QrCode\Facades\QrCode::format('png')->size(80)->margin(0)->generate($qrContent)) !!} " style="width: 18mm; height: 18mm;">
+                <img src="data:image/png;base64, {!! base64_encode(\SimpleSoftwareIO\QrCode\Facades\QrCode::format('png')->size(80)->margin(0)->generate($qrContent)) !!} ">
             </div>
             <div class="terms">
                 <strong>Terms & Conditions</strong>
